f72444a03472fe4a189ee92d56a6fb02636f2d66
ANGIELOUX <angelajohnson943@gmail.com>
2022-07-25 16:38:13 +1000

    Create product (id) routes testing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get '/products', to: 'products#index', as: 'products'
+  get "/products", to: "products#index", as: "products"
 
-  get '/orders', to: 'orders#index', as: 'orders'
+  get "/orders", to: "orders#index", as: "orders"
+
+  get "/products/:id", to: "products#show", as: "product"
 end
--- spec/requests/products_spec.rb
+++ spec/requests/products_spec.rb
@@ -1,27 +1,62 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe "Products", type: :request do
+  before(:all) do
+    create(:product)
+  end
+
   describe "GET /products" do
     before(:each) do
-      @product = create(:product)
-    end
-    
-    before(:each) do
       ## Test the response to the get request
-      get '/products' 
-
+      get "/products"
     end
 
-    it "should respond with 200 ok" do 
+    it "should respond with 200 ok" do
       expect(response).to have_http_status(200)
     end
 
-    it "should respond with json" do 
+    it "should respond with json" do
       expect(response.content_type).to eq("application/json; charset=utf-8")
     end
 
-    it "should include the right content" do 
-      expect(response.body).to include('Black Hawk')
+    it "should include the right content" do
+      expect(response.body).to include("Black Hawk")
+    end
+  end
+
+  describe "GET /products/:id" do
+    context "valid product id" do
+      before(:each) do
+        get "/products/1"
+      end
+      it "should respond with 200 ok" do
+        expect(response).to have_http_status(200)
+      end
+
+      it "should respond with json" do
+        expect(response.content_type).to eq("application/json; charset=utf-8")
+      end
+
+      it "should include the right content" do
+        expect(response.body).to include("Black Hawk")
+      end
+    end
+
+    context "invalid product id" do
+      before(:each) do
+        get "/products/2"
+      end
+      it "should respond with 404" do
+        expect(response).to have_http_status(404)
+      end
+
+      it "should respond with json" do
+        expect(response.content_type).to eq("application/json; charset=utf-8")
+      end
+
+      it "should not have content" do
+        expect(response.body).to include("Unable to find product")
+      end
     end
   end
 end
