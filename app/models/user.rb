ced888864729b01d7ef489475e588d2acec58a24
ANGIELOUX <angelajohnson943@gmail.com>
2022-08-07 19:48:57 +1000

    Fix type in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: true
   validates :username, presence: true, uniqueness: true
-  validates_format_of :username, with: /^[^@]+$/, multiline: true, message: "Username cannot contain @ symbol"
-  validates :email, presence: true, confirmation: true
+  validates_format_of :username, with: /^[^@]+$/, multiline: true, message: "cannot contain @ symbol"
+  validates :password, presence: true, confirmation: true
   validates :password_confirmation, presence: true
 
   scope :login, ->(input) { User.where(username: input).or(User.where(email: input)) }
